8f19869da87014e06e5f094a78f6149e43a223c5
Awakenight <114548230+Awakenight@users.noreply.github.com>
2022-10-05 15:49:26 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+}
